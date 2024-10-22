8379c9684af5309964ba82ce94cfbf0e1c460962
Abhishek Singh <abhishekkumar60818@gmail.com>
2024-10-23 02:51:58 +0530

    Add project files.

---
+++ ASPCoreWebAPICRUD/Models/CodeFirstDbContext.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreWebAPICRUD.Models;
+
+public partial class CodeFirstDbContext : DbContext
+{
+    public CodeFirstDbContext()
+    {
+    }
+
+    public CodeFirstDbContext(DbContextOptions<CodeFirstDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    public virtual DbSet<UserTbl> UserTbls { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.GenderName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<UserTbl>(entity =>
+        {
+            entity.ToTable("user_tbl");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.Gender)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("name");
+            entity.Property(e => e.Password)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
